44228abaeb75de8f035d8243689da3c70dfda2c9
rifkiay <rifkiainul422@gmail.com>
2025-06-15 17:39:15 +0700

    no acne predict

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('predict/', include('prediction.urls')),
     path('', include('home.urls')),
 ]
 
